ccbf23531d1c8c9746cd411482523239365ace9d
Aman Singhal <amansinghal232@gmail.com>
2022-07-25 13:49:54 +0530

    adding files of whiteboard

---
+++ script.js
@@ -0,0 +1,33 @@
+const canvas = document.querySelector('#draw');
+const ctx = canvas.getContext('2d');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = 'black';
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.lineWidth = 5;
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+function draw(e){
+  if(!isDrawing){
+    return;
+  }
+  console.log(e);
+  ctx.beginPath();
+  ctx.moveTo(lastX,lastY);
+  ctx.lineTo(e.offsetX,e.offsetY);
+  ctx.stroke();
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+}
+canvas.addEventListener('mousedown',(e) => {
+  isDrawing = true;
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+
+
+});
+canvas.addEventListener('mousemove',draw);
+canvas.addEventListener('mouseup',()=>isDrawing = false);
+canvas.addEventListener('mouseout',()=>isDrawing = false);
